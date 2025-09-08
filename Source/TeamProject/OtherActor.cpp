3404686db9a4136ddf325ce08e894c3aef13093a
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-09-08 15:58:40 +0900

    change actors

--- Source/TeamProject/OtherActor.cpp
+++ Source/TeamProject/OtherActor.cpp
@@ -1,24 +1,7 @@
 #include "OtherActor.h"
 
-// Sets default values
 AOtherActor::AOtherActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AOtherActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AOtherActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+	PrimaryActorTick.bCanEverTick = false;
 }
 
